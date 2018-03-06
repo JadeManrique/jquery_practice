84a4114c201eef9e7924404753f6f6185f145114
Jade Manrique <jade.a.manrique@gmail.com>
2018-03-05 16:05:32 -0800

    fixed jQuery errors

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,23 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-console.log("jQwerty!")
+
+
+  $("#text").click( function () {
+
+    var currentColor = $("#header").css("color");
+
+    if (currentColor == "rgb(0, 0, 0)") {
+
+      $("#header").css("color", "gold");
+
+    } else {
+
+      $("#header").css("color", "rgb(0, 0, 0)");
+
+    }
+
+  });
+
+
 
 });
