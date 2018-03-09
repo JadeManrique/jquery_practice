fb7254a4df0ff6ba99f58ff65bdac74affd943a4
Jade Manrique <jade.a.manrique@gmail.com>
2018-03-09 12:00:42 -0800

    Updated everything

--- scripts/scripts2.js
+++ scripts/scripts2.js
@@ -5,7 +5,24 @@ $(document).ready(function () {
     var emailLog = $("#email").val();
     var passwordLog = $("#password").val();
 
-    console.log(emailLog, passwordLog);
+    console.log( validateEmail(emailLog), passwordLog);
+
+    if (emailLog == "jade.a.manrique@gmail.com") {
+      if (passwordLog == "12345678") {
+        console.log("Correct");
+      } else {
+        console.log("Error");
+      }
+
+    } else {
+        console.log("No user with email " + emailLog + " exists.");
+      }
+    });
+
   });
 
-});
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
